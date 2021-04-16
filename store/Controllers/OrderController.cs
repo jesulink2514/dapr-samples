a6ffea0899ca03443d09296e96c033efd3babde1
jesulink2514 <jesus@somostechies.com>
2021-04-15 23:17:35 -0500

    store begin

---
+++ store/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using store.Models;
+
+namespace store.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly DaprClient daprClient;
+        private readonly ILogger<OrderController> _logger;
+        public const string StoreName = "";
+        public OrderController(DaprClient daprClient, ILogger<OrderController> logger)
+        {
+            this.daprClient = daprClient;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<Order> Get()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewOrder(Order newOrder)
+        {
+
+            return CreatedAtAction(nameof(Get), newOrder);
+        }
+    }
+}
---
+++ store/Models/Order.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace store.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
